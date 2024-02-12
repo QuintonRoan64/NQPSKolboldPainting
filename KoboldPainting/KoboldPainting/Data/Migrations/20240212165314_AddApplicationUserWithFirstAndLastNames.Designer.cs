10fa0543eb7d0ec2dfc3832912c1cd60578b51b9
Nathaniel Kuga <natelkuga@protonmail.com>
2024-02-12 10:28:43 -0800

    NK: working on the Application user setup.

--- KoboldPainting/KoboldPainting/Data/Migrations/20240212165314_AddApplicationUserWithFirstAndLastNames.Designer.cs
+++ KoboldPainting/KoboldPainting/Data/Migrations/20240212165314_AddApplicationUserWithFirstAndLastNames.Designer.cs
@@ -4,6 +4,7 @@ using KoboldPainting.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KoboldPainting.Data.Migrations
 {
     [DbContext(typeof(KoboldPaintingIdentityDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240212165314_AddApplicationUserWithFirstAndLastNames")]
+    partial class AddApplicationUserWithFirstAndLastNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
